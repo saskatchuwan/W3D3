fbeadd7cc844b063922e287a2ca855a4570ecabd
App Academy Student <appacademy@Mac-mini.local>
2019-01-23 14:35:41 -0800

    Initial commit for associations exercises

---
+++ Associations_exercise/app/models/course.rb
@@ -0,0 +1,38 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint(8)        not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
+class Course < ApplicationRecord
+
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
+
+  has_many :enrolled_students, through: :enrollments, source: :user
+
+
+  #self-referential association
+  belongs_to :prerequisite,
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    class_name: :Course,
+    optional: true  #should include this to account for instances that might not hav ea pre-req (validation)
+
+  #example of an association without an accompanying counterpart
+  #can have one without the other.
+  belongs_to :instructor,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :User
+    
+
+end
